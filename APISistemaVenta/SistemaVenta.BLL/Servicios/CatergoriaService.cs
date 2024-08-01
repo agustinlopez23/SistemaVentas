a2ed59ab2d219ce2bddde891992401245b50e94d
Agustin Windows <agustinlopezdeveloper@gmail.com>
2024-07-31 23:06:14 -0300

    bussines

---
+++ APISistemaVenta/SistemaVenta.BLL/Servicios/CatergoriaService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class CatergoriaService :ICategoriaService
+    {
+        private readonly IGenericRepository<Categoria> _categoriaRepositorio;
+        private readonly IMapper _mapper;
+
+        public CatergoriaService(IGenericRepository<Categoria> categpriaRepositorio, IMapper mapper)
+        {
+            _categoriaRepositorio = categpriaRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoriaDTO>> Lista()
+        {
+            try
+            {
+                var listaCategorias = await _categoriaRepositorio.Consultar();
+                return _mapper.Map<List<CategoriaDTO>>(listaCategorias.ToList());
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
